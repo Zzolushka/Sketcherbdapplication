b779e6dec13e5eaf7cf1e9a93295329e521b40d8
mashineforkill <mashineforkill@gmail.com>
2018-10-07 22:53:21 +0300

    firstupdate

---
+++ Sketcher/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Sketcher.Controllers
+{
+    public class AccountController : Controller
+    {
+        Models.UserContext db = new Models.UserContext();
+        // GET: Account
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(FormCollection collection)
+        { 
+            Models.User user = new Models.User() { UserLogin = collection["UserLogin"], UserPassword = collection["UserPassword"], UserPhotoPath = "http://econet.ru/assets/noavatar.png" };
+            db.Users.Add(user);
+            db.SaveChanges();
+            FormsAuthentication.SetAuthCookie(collection["UserLogin"], true);
+            return RedirectToAction("index","home");
+        }
+
+        [Authorize]
+        public ActionResult ShowUserInformation()
+        {
+            var currentUser = db.Users.FirstOrDefault(u=>u.UserLogin == User.Identity.Name);
+            return View(currentUser);
+        }
+
+        public ActionResult ShowAllUsers()
+        {
+            return View(db.Users);
+        }
+    }
+}
---
+++ Sketcher/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sketcher.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserLogin { get; set; }
+        public string UserPassword { get; set; }
+        public string UserPhotoPath { get; set; }
+    }
+
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class UserContext : DbContext
+    {
+        public UserContext()
+            : base("UserContext")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
